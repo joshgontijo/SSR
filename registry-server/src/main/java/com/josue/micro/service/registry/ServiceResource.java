905660afb2dbefc11dc1cec39b02595e16aa55da
Josue Gontijo <josue.eduardo206@gmail.com>
2016-06-14 23:59:35 +0100

    Removed unused servlet request

--- registry-server/src/main/java/com/josue/micro/service/registry/ServiceResource.java
+++ registry-server/src/main/java/com/josue/micro/service/registry/ServiceResource.java
@@ -8,7 +8,6 @@ import com.hazelcast.map.EntryProcessor;
 import javax.annotation.PostConstruct;
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
-import javax.servlet.http.HttpServletRequest;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
@@ -16,7 +15,6 @@ import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
-import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import java.io.Serializable;
@@ -44,9 +42,6 @@ public class ServiceResource implements Serializable {
 
     @Inject
     private IMap<String, ServiceConfig> cache;
-
-    @Context
-    private HttpServletRequest request;
 
     @PostConstruct
     public void init() {
