3803ec8fe4ff1ceecd0330c9f374f73304029cc3
Josue Gontijo <josue.eduardo206@gmail.com>
2016-08-21 16:23:18 +0100

    Set default port to 9000 by using swarm config files

--- registry-server/src/main/java/com/josue/micro/service/registry/Main.java
+++ registry-server/src/main/java/com/josue/micro/service/registry/Main.java
@@ -9,6 +9,8 @@ public class Main {
 
     public static void main(String[] args) throws Exception {
         Swarm swarm = new Swarm();
+        swarm.withStageConfig(Main.class.getClassLoader().getResource("config.yml"));
+
         swarm.start();
         swarm.deploy();
     }
